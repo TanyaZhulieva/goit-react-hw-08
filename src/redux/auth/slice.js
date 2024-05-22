1a29233990a3a9994d602611d9e2865277910df8
Тетяна Жульєва <128372881+TanyaZhulieva@users.noreply.github.com>
2024-05-22 18:27:05 +0200

    upd

--- src/redux/auth/slice.js
+++ src/redux/auth/slice.js
@@ -39,6 +39,8 @@ const authSlice = createSlice({
         state.user = action.payload;
         state.isLoggedIn = true;
         state.isRefreshing = false;
+      }).addCase(refreshUser.rejected, (state)=>{
+        state.isRefreshing = false;
       }),
 });
 
